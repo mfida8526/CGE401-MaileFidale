d9d6aba29c4f1efb65c783382d97f375ba6eb05a
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-07 20:58:38 -0500

    player controller script

---
+++ CGE401-Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+
+    public float forwardInput;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //move player forward with forward Input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //rotate player with horizontal Input
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
