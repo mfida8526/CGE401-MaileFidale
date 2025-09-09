c5dbaca3be8eadd62f71068a8acbba45ea6cbfe3
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-08 21:30:05 -0500

    scripts

---
+++ CGE401-Challenge1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float rotationSpeed = 360f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f,0f,rotationSpeed * Time.deltaTime);
+    }
+}
