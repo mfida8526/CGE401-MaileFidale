bd7912e96e77dc33aba50d4aabe70c075da48d9b
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-10 12:49:53 -0500

    pro2 up to DestroyOutOfBounds script

---
+++ CGE401-Prototype2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attach to food projectil prefab
+public class DetectCollisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
