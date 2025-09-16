7237140a38378b19d0a85ef18349cc415a8b3e18
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-16 18:40:32 -0500

    started health progress

--- CGE401-Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ CGE401-Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -13,20 +13,42 @@ public class SpawnManagerX : MonoBehaviour
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
 
+    public HealthSystem healthSystem;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        healthSystem = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+
+        StartCoroutine(SpawnRandomBallCoroutine());
+    }
+
+    IEnumerator SpawnRandomBallCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (!healthSystem.gameOver)
+        {
+            SpawnRandomBall();
+
+            float randomDelay = Random.Range(3.0f, 5.0f);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        //pick a random ball index
+        int prefabIndex = Random.Range(0, ballPrefabs.Length);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[prefabIndex], spawnPos, ballPrefabs[prefabIndex].transform.rotation);
     }
 
 }
