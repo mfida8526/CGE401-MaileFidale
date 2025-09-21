ae8be17b8efd39a3179c452cb5c6b58006ceb55d
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-21 13:40:05 -0500

    import pro3 assets

--- CGE401-Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ CGE401-Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -13,6 +13,8 @@ public class PlayerControllerX : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+
         }
     }
 }
