10e58ff957e85880b53bd143a9768450140b23fe
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-21 14:42:29 -0500

    moving background

--- CGE401-Prototype3/Assets/Scripts/MoveLeft.cs
+++ CGE401-Prototype3/Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    public float speed = 30f;
+    private float leftBound = -15;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,14 @@ public class MoveLeft : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        //if we are out fo bounds to the left and the gameObject is an obstacle then destroy gameObject
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ CGE401-Prototype3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //save the starting position as a Vector3
+        startPosition = transform.position;
+
+        //set the repeatwidth to half of the width of the background
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if background is further to the left than the repeatWidth, reset it to its startPosition
+        if(transform.position.x < startPosition.x - repeatWidth)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
