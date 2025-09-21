44a7bda04e18f20f4145a00c172a239522af21fc
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-21 14:13:20 -0500

    player controller script

--- CGE401-Prototype3/Assets/Scripts/PlayerController.cs
+++ CGE401-Prototype3/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    public float jumpForce;
+    public ForceMode forceMode;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    
     void Start()
     {
-        
+        //set a reference to our rigidbody component
+        rb = GetComponent<Rigidbody>();
+
+        forceMode = ForceMode.Impulse;
+
+        //modify gravity
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce, forceMode);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
