7913aa4637a0bbf0ff09151a909c5fe5f21c90ec
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-21 15:08:08 -0500

    UIManager

---
+++ CGE401-Prototype3/Assets/Scripts/UIManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Text scoreText;
+    public int score = 0;
+
+    public PlayerController playerControllerScript;
+
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+        if (playerControllerScript == null)
+        {
+            playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        }
+
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //display score until game is over
+        if (!playerControllerScript.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+
+        //loss condition: hit the obstacle
+        if(playerControllerScript.gameOver && !won)
+        {
+            scoreText.text = "You Lose! \nPress R to Try Again!";
+        }
+
+        //win condition: 10 points
+        if (score >= 10)
+        {
+            playerControllerScript.gameOver = true;
+            won = true;
+
+            //stop player running
+
+
+            scoreText.text = "You Win! \nPress R to Try Again!";
+        }
+
+        if(playerControllerScript.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetACtiveScene().name);
+        }
+    }
+}
