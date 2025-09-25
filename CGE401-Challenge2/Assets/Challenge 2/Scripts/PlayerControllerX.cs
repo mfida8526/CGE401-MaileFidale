fe4c6e6c793e977041a8db1ec1062e06b674b7c3
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-25 13:44:31 -0500

    boolean and coroutine for spawn dog

--- CGE401-Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ CGE401-Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,15 +6,26 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    public float spawnCooldown = 2f;  // cooldown time in seconds
+
+    private bool canSpawn = true;     // can spawn initially
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
 
-
+            StartCoroutine(SpawnCooldownRoutine());
         }
+    }
+
+    IEnumerator SpawnCooldownRoutine()
+    {
+        canSpawn = false;                  // prevent spawning again immediately
+        yield return new WaitForSeconds(spawnCooldown);  // wait cooldown time
+        canSpawn = true;                   // allow spawning again
     }
 }
