9bfba35c638712037c4b9927ac4d3366e86f84ea
mfida8526 <mfida8526@365.elmhurst.edu>
2025-09-26 23:13:32 -0500

    challenge3 pt2 requirements unfinished

---
+++ CGE401-Challenge3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed;
+    private PlayerControllerX playerControllerScript;
+    private float leftBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If game is not over, move to the left
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+
+        // If object goes off screen that is NOT the background, destroy it
+        if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+        {
+            Destroy(gameObject);
+        }
+
+        // Stop physics-based movement
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ CGE401-Challenge3/Assets/Challenge 3/Scripts/ScoreManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text gameOverText;
+    public int score = 0;
+
+    public PlayerControllerX playerControllerXScript;
+
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+        if (playerControllerXScript == null)
+        {
+            playerControllerXScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+        }
+
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //display score until game is over
+        if (!playerControllerXScript.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+
+        //win condition: 10 points
+        if (score >= 10)
+        {
+            playerControllerXScript.gameOver = true;
+            won = true;
+
+            //stop player running
+
+
+            scoreText.text = "You Win! \nPress R to Try Again!";
+        }
+
+        if (playerControllerXScript.gameOver)
+        {
+            gameOverText.gameObject.SetActive(true);
+        }
+
+        if (playerControllerXScript.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+    }
+}
