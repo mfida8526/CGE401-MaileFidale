b5c5a49840673b1c429045e6fec9f9e66df16347
mfida8526 <mfida8526@365.elmhurst.edu>
2025-11-10 21:27:37 -0600

    copy of assignment 5b

---
+++ CGE401-Assignment6/Assets/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+    public ParticleSystem muzzleFlash;
+    public float hitForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //at the beginning of the Shoot() method, play the particle effect
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+            //get the Target script off the hit object
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            //If a target script was found, make the target take damage
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+
+                //if the shot hits a rigidbody, apply a force
+                if(hitInfo.rigidbody != null)
+                {
+                    hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
