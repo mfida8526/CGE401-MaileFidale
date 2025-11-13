19b0ee2017f3c81c465d615747638d8399614b86
mfida8526 <mfida8526@365.elmhurst.edu>
2025-11-13 10:38:04 -0600

    weapons, rifle, shotgun, pistol weapon switcher code

---
+++ CGE401-Assignment6/Assets/Scripts/Pistol.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapons
+{    
+    private void Start()
+    {
+        weaponName = "Pistol";
+    }
+}
---
+++ CGE401-Assignment6/Assets/Scripts/Weapons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapons : MonoBehaviour, IWeapon
+{
+    [SerializeField] protected string weaponName;
+
+    // Public getter so other scripts can read the name
+    public string WeaponName => weaponName;
+
+    public virtual void Activate()
+    {
+        gameObject.SetActive(true);
+        Debug.Log(weaponName + " activated");
+    }
+
+    public virtual void Deactivate()
+    {
+        gameObject.SetActive(false);
+        Debug.Log(weaponName + " deactivated");
+    }
+}
