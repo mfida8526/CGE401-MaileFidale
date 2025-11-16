145fe6b893f38013c539d023e983f286b5ca6ce2
mfida8526 <mfida8526@365.elmhurst.edu>
2025-11-15 23:10:19 -0600

    player movement

---
+++ CGE401-Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");        
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+    }
+}
