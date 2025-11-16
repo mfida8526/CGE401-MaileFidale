ef937ea5e6e8784a6e044a99e17ad0a6529f4714
mfida8526 <mfida8526@365.elmhurst.edu>
2025-11-16 02:51:51 -0600

    powerup

--- CGE401-Prototype4/Assets/Scripts/PlayerController.cs
+++ CGE401-Prototype4/Assets/Scripts/PlayerController.cs
@@ -9,20 +9,66 @@ public class PlayerController : MonoBehaviour
 
     private float forwardInput;
 
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+
+    public GameObject powerupIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("FocalPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
-        forwardInput = Input.GetAxis("Vertical");        
+        forwardInput = Input.GetAxis("Vertical");
+
+        //move our powerup indicator to the ground below our player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void FixedUpdate()
     {
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Player collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+
+            //get local reference to the enemy rb
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+            //set a Vector3 with a direction away from the player
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position).normalized;
+
+            //add force away from player
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
